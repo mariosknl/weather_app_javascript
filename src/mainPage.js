b17bb6f2e8013ad8898c6acbea349c70ffb7aca6
mariosknl <mariosknl@outlook.com>
2020-07-14 11:20:10 +0300

    center Buttons

--- src/mainPage.js
+++ src/mainPage.js
@@ -6,17 +6,19 @@ const generate = () => {
   const { mainContainer } = elements.el();
   mainContainer.innerHTML = `
   <div class="mx-auto pt-16 w-full max-w-xs">
-  <form class="bg-white bg-blue-100 bg-opacity-75 rounded shadow-md px-8 pt-6 pb-8 mb-4 text-center" id="searchForm">
-  <div class="mb-4">
-  <p class="loading">Loading</p>
-  <label class="block text-gray-700 text-sm font-bold mb-2 for="citySearch">
-  Write your city and get its temperature.
-  </label>
-  <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="citySearch" type="text">
-  </div>
-  </form>
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" id="celcius-btn">Celcius</button>
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" id="fahr-btn">Farhneit</button>
+    <form class="bg-white bg-blue-100 bg-opacity-75 rounded shadow-md px-8 pt-6 pb-8 mb-4 text-center" id="searchForm">
+      <div class="mb-4">
+      <p class="loading">Loading</p>
+      <label class="block text-gray-700 text-sm font-bold mb-2 for="citySearch">
+      Write your city and get its temperature.
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="citySearch" type="text">
+      </div>
+    </form>
+    <div class="mx-auto w-full max-w-xs flex justify-center">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-1 rounded-full" id="celcius-btn">Celcius</button>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-1 rounded-full" id="fahr-btn">Farhneit</button>
+    </div>
   </div>
   `;
   const { celcBtn, fahrBtn } = elements.el();
